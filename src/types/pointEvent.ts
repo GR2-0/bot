3515cfe8108d6329bbee775b7d7e63995361006e
wldwldcrd <wldwldcrd@gmail.com>
2025-08-24 23:44:12 +0300

    Clean-up all previous files. Update .gitignore and README

--- src/types/pointEvent.ts
+++
@@ -1,7 +0,0 @@
-export interface PointEvent {
-  userId: number;
-  type: 'meetup_attendance' | 'meetup_speaker' | 'custom';
-  meetupId?: string;  // можно без, если тип не про митап
-  points: number;
-  timestamp: string;
-}
